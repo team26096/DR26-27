a0410e37126b4ddf5640e6536f1be646c4d9b22a
vijay94210 <94215814+vijay94210@users.noreply.github.com>
2026-09-01 17:12:41 -0400

    Committing run1 right base which now includes humongous fungus and forest elder tree

--- run1.py
+++ run1.py
@@ -1,61 +1,69 @@
-# to execute this code from terminal use the following command
+# to load this code from terminal use the following command
 # py -3 -m pybricksdev run ble .\run1.py --no-start --name pixiebricks
 
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor
 from pybricks.parameters import Port, Direction, Stop
 from pybricks.robotics import DriveBase
+from pybricks.tools import StopWatch
 import sys
+
+timer = StopWatch()
 
 hub = PrimeHub()
 
 # Drive motors
-left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
-right_motor = Motor(Port.E, Direction.CLOCKWISE)
-
-# Attachment motor
-motor_c = Motor(
-    Port.C,
-    gears=[
-        [20, 12],
-        [12, 36],
-        [12, 20]
-    ]
-)
-
+left_drive = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right_drive = Motor(Port.E, Direction.CLOCKWISE)
 drivebase = DriveBase(
-    left_motor,
-    right_motor,
+    left_drive,
+    right_drive,
     wheel_diameter=62.4,
     axle_track=164
 )
-
 drivebase.use_gyro(True)
-
 current_settings = drivebase.settings()
 print("Robot Settings:", current_settings)
+print("Robot max",drivebase.distance_control.limits())
 
-# Drive forward 55 cm
-drivebase.straight(500)
-drivebase.settings(20,100,125,500)
-drivebase.straight(100)
-
-#drivebase.turn(-93)
-
-motor_c.run_angle(
-    speed=360,
-    rotation_angle=55,
-    then=Stop.HOLD,
-    wait=True
+# Attachment motor
+left_attach = Motor(
+    Port.B,
+    gears=[
+        [12, 20],
+        [12, 36],
+    ]
 )
 
-motor_c.run_angle(
-    speed=360,
-    rotation_angle=-75,
-    then=Stop.HOLD,
-    wait=True
+right_attach = Motor(
+    Port.C,
+    gears=[
+        [12, 20],
+        [12, 36],
+    ]
 )
 
-# Drive forward Backward10 cm
-# drivebase.straight(-480)
+print("Attachment motor max:", left_attach.control.limits())
 
+# drivebase.settings(100,100,125,500)
+# Drive - drivebase.straight()
+# Turn - drivebase.turn()
+# Attachment - left_attach.run_angle(speed=360, rotation_angle=55, then=Stop.HOLD, wait=True)
+# Attachment - right_attach.run_angle(speed=360, rotation_angle=55, then=Stop.HOLD, wait=True)
+# Humoungous Fungus
+#left_attach.reset_angle(0)
+#print(left_attach.angle())
+#left_attach.run_angle(speed=1030, rotation_angle=-26.5, then=Stop.HOLD, wait=False)
+#drivebase.straight(750)
+#left_attach.run_angle(speed=1030, rotation_angle=-90, then=Stop.HOLD, wait=True)
+#print(left_attach.angle())
+#Forest Elder
+
+#drivebase.turn(90)
+#left_attach.run_angle(speed=1030, rotation_angle=116, then=Stop.HOLD, wait=True)
+#print(left_attach.angle())
+#drivebase.straight(80)
+#left_attach.run_angle(speed=1030, rotation_angle=-45, then=Stop.HOLD, wait=True)
+
+elapsed_seconds = timer.time() / 1000
+print("Elapsed time: {:.2f} seconds".format(elapsed_seconds))
